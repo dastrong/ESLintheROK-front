c2f4c4ced15b9c349e0a9f8566e9671da58ad04b
dastrong <danielstrong.ds@gmail.com>
2018-10-14 19:05:13 +0900

    v1.4.2 edited tooltip

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -36,50 +36,52 @@ class GamePage extends PureComponent {
     return (
       <div className='gamePage-container'>
         <img src={game.info.images.topText} alt='game logo'></img>
-        <div className='gamePage-buttons'>
-          <Button  
-            as={Link}
-            to={{
-              pathname: `${game.router.path}/teacher`,
-              state: { pageTransition:'slideRight' }
-            }}
-            className='left-btn outer-btn huge'
-            color='blue'
-            icon labelPosition='left'
-          >
-            <Icon size='large' name='angle left' /> 
-            TEACHER
-            <br></br>
-            <span>INSTRUCTIONS</span>
-          </Button>
-          <Button
-            as={Link}
-            to={{
-              pathname: `${game.router.path}/student`,
-              state: { pageTransition:'slideLeft' }
-            }}
-            className='right-btn outer-btn huge'
-            color='blue'
-            icon labelPosition='right'
-          >
-            STUDENT 
-            <br></br>
-            <span>INSTRUCTIONS</span>
-            <Icon size='large' name='angle right' />
-          </Button>
-          <Button className='disabled-backer start-btn circular'></Button>
-          <Button  
-            as={Link}
-            to={{
-              pathname: `${game.router.path}/start`,
-              state: { pageTransition:'slideUp' }
-            }}
-            className={startBtn}
-            color='green'
-          >
-            START
-          </Button>
+        <div>
           {tooltip}
+          <div className='gamePage-buttons'>
+            <Button  
+              as={Link}
+              to={{
+                pathname: `${game.router.path}/teacher`,
+                state: { pageTransition:'slideRight' }
+              }}
+              className='left-btn outer-btn huge'
+              color='blue'
+              icon labelPosition='left'
+            >
+              <Icon size='large' name='angle left' /> 
+              TEACHER
+              <br></br>
+              <span>INSTRUCTIONS</span>
+            </Button>
+            <Button
+              as={Link}
+              to={{
+                pathname: `${game.router.path}/student`,
+                state: { pageTransition:'slideLeft' }
+              }}
+              className='right-btn outer-btn huge'
+              color='blue'
+              icon labelPosition='right'
+            >
+              STUDENT 
+              <br></br>
+              <span>INSTRUCTIONS</span>
+              <Icon size='large' name='angle right' />
+            </Button>
+            <Button className='disabled-backer start-btn circular'></Button>
+            <Button  
+              as={Link}
+              to={{
+                pathname: `${game.router.path}/start`,
+                state: { pageTransition:'slideUp' }
+              }}
+              className={startBtn}
+              color='green'
+            >
+              START
+            </Button>
+          </div>
         </div>
       </div>
     )
