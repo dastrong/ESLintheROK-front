fa8a60453ec91556e44c036096e13b73e5d5cf58
dastrong <danielstrong.ds@gmail.com>
2018-10-14 11:02:58 +0900

    v1.3.2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import './index.css';
-require('dotenv').config();
 
 const Site = () => (
   <Router>
