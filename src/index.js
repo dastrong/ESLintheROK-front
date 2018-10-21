96914c1741370ca2585668c7f5816f1b65c2d51d
dastrong <danielstrong.ds@gmail.com>
2018-10-21 15:58:55 +0900

    v1.5.12 again enabled service worker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +9,10 @@ import ConfirmBox from './components/reusable/ConfirmBox';
 import './index.css';
 
 
-const handleSiteUpdate = () => true;
+const handleSiteUpdate = () => {
+  console.log('site updated')
+  return true;
+}
 
 class Site extends Component {
   state = {isSiteUpdated: false || handleSiteUpdate}
@@ -17,21 +20,24 @@ class Site extends Component {
   // siteUpdated = () => this.setState({ isSiteUpdated: true });
 
   render(){
+    console.log(this.state)
     return (
       <Router>
-        <ConfirmBox
-          open={this.state.isSiteUpdated} 
-          header="The site has been updated"
-          content={<p style={{padding: '10px'}}>Please refresh your browser to stay current</p>}
-          confirmText="Refresh"
-          cancelText="I don't like nice things."
-          onConfirm={()=> window.location.reload()}
-        />
-        {window.innerWidth < 768
-          ? <MobileError /> 
-          : !!document.documentMode
-            ? <IEError />
-            : <App /> }
+        <Fragment>
+          <ConfirmBox
+            open={this.state.isSiteUpdated} 
+            header="The site has been updated"
+            content={<p style={{padding: '10px'}}>Please refresh your browser to stay current</p>}
+            confirmText="Refresh"
+            cancelText="I don't like nice things."
+            onConfirm={()=> window.location.reload()}
+          />
+          {window.innerWidth < 768
+            ? <MobileError /> 
+            : !!document.documentMode
+              ? <IEError />
+              : <App /> }
+        </Fragment>
       </Router>
     )
   }
