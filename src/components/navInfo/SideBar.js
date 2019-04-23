54d181a98453d103109c4840626da1bd5204dae3
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-23 21:40:59 +0900

    changed SideBar to utilize the new Context Store

--- src/components/navInfo/SideBar.js
+++ src/components/navInfo/SideBar.js
@@ -1,128 +1,126 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Menu, Sidebar } from "semantic-ui-react";
 import { games } from "../../helpers/data";
+import { useStore } from "../../store";
 import "../../styles/navInfo/SideBar.css";
 
-class SideBar extends Component {
-  openAndClose = name => {
-    this.props.openDataModal(name);
-    this.props.closeSideBar();
-  };
+export default function SideBar(props) {
+	const [{ showSideBar, isGameReady }, dispatch] = useStore();
 
-  render() {
-    const {
-      opacity,
-      isGameReady,
-      showSideBar,
-      openSideBar,
-      closeSideBar
-    } = this.props;
-    return (
-      <div className="sidebar sidebar-main">
-        <Button
-          className="sideBarBtn"
-          icon="list"
-          size="massive"
-          onClick={openSideBar}
-          style={{ opacity }}
-        />
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          icon="labeled"
-          inverted
-          onHide={closeSideBar}
-          vertical
-          visible={showSideBar}
-          width="wide"
-        >
-          <Menu.Item className="home-close-btn-group">
-            <Button.Group size="massive">
-              <Button
-                basic
-                inverted
-                size="massive"
-                as={Link}
-                to={{
-                  pathname: "/",
-                  state: { pageTransition: "slideRight" }
-                }}
-                color="blue"
-                icon="home"
-                onClick={closeSideBar}
-              />
-              <Button
-                basic
-                inverted
-                color="red"
-                icon="x"
-                onClick={closeSideBar}
-              />
-            </Button.Group>
-          </Menu.Item>
-          <Menu.Item className="lesson-btns">
-            <Button.Group>
-              <Button
-                basic
-                inverted
-                onClick={() => this.openAndClose("lessons")}
-                color="green"
-              >
-                <Icon size="big" name="book" />
-                Lessons
-              </Button>
-              <Button
-                basic
-                inverted
-                onClick={() => this.openAndClose("data")}
-                color="yellow"
-              >
-                <Icon size="big" name="cogs" />
-                Custom
-              </Button>
-              <Button
-                basic
-                inverted
-                onClick={() => this.openAndClose("dataEdit")}
-                color="orange"
-                disabled={!isGameReady}
-              >
-                <Icon size="big" name="edit" />
-                Edit Data
-              </Button>
-            </Button.Group>
-          </Menu.Item>
-          {/* creates our menu links, if the game has been marked completed */}
-          {games
-            .filter(({ info }) => info.completed)
-            .map(({ router, info }) => (
-              <MenuItem
-                key={router.path}
-                path={router.path}
-                icon={router.icon}
-                title={info.title}
-                closeSideBar={closeSideBar}
-              />
-            ))}
-        </Sidebar>
-      </div>
-    );
-  }
+	function openAndClose(e, { name }) {
+		dispatch({ type: "openDataModal", name });
+		dispatch({ type: "closeSideBar" });
+	}
+
+	const closeSideBar = () => dispatch({ type: "closeSideBar" });
+
+	const { opacity } = props;
+	return (
+		<div className="sidebar sidebar-main">
+			<Button
+				className="sideBarBtn"
+				icon="list"
+				size="massive"
+				onClick={() => dispatch({ type: "openSideBar" })}
+				style={{ opacity }}
+			/>
+			<Sidebar
+				as={Menu}
+				vertical
+				inverted
+				width="wide"
+				icon="labeled"
+				animation="overlay"
+				visible={showSideBar}
+				onHide={closeSideBar}
+			>
+				<Menu.Item className="home-close-btn-group">
+					<Button.Group size="massive">
+						<Button
+							basic
+							inverted
+							size="massive"
+							as={Link}
+							to={{
+								pathname: "/",
+								state: { pageTransition: "slideRight" },
+							}}
+							color="blue"
+							icon="home"
+							onClick={closeSideBar}
+						/>
+						<Button
+							basic
+							inverted
+							color="red"
+							icon="x"
+							onClick={closeSideBar}
+						/>
+					</Button.Group>
+				</Menu.Item>
+				<Menu.Item className="lesson-btns">
+					<Button.Group>
+						<Button
+							basic
+							inverted
+							name="lessons"
+							onClick={openAndClose}
+							color="green"
+						>
+							<Icon size="big" name="book" />
+							Lessons
+						</Button>
+						<Button
+							basic
+							inverted
+							name="data"
+							onClick={openAndClose}
+							color="yellow"
+						>
+							<Icon size="big" name="cogs" />
+							Custom
+						</Button>
+						<Button
+							basic
+							inverted
+							name="dataEdit"
+							onClick={openAndClose}
+							color="orange"
+							disabled={!isGameReady}
+						>
+							<Icon size="big" name="edit" />
+							Edit Data
+						</Button>
+					</Button.Group>
+				</Menu.Item>
+				{/* creates our menu links, if the game has been marked completed */}
+				{games
+					.filter(({ info }) => info.completed)
+					.map(({ router, info }) => (
+						<MenuItem
+							key={router.path}
+							path={router.path}
+							icon={router.icon}
+							title={info.title}
+							closeSideBar={closeSideBar}
+						/>
+					))}
+			</Sidebar>
+		</div>
+	);
 }
 
-export default SideBar;
-
 const MenuItem = ({ path, icon, title, closeSideBar }) => (
-  <Menu.Item
-    as={Link}
-    onClick={closeSideBar}
-    to={{
-      pathname: `${path}`,
-      state: { pageTransition: "slideLeft" }
-    }}
-  >
-    <Icon name={icon} />
-    {title}
-  </Menu.Item>
+	<Menu.Item
+		as={Link}
+		onClick={closeSideBar}
+		to={{
+			pathname: `${path}`,
+			state: { pageTransition: "slideLeft" },
+		}}
+	>
+		<Icon name={icon} />
+		{title}
+	</Menu.Item>
 );
