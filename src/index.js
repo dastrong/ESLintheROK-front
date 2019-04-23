f958055f0820cb6c11f906f04811a01cdb7caa10
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-23 18:25:11 +0900

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
-import App from './components/App';
-import MobileError from './components/MobileError';
-import IEError from './components/IEError';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App";
+import MobileError from "./components/MobileError";
+import IEError from "./components/IEError";
+import "./index.css";
 
 const Site = () => (
-  <Router>
-    {window.innerWidth < 768
-      ? <MobileError /> 
-      : !!document.documentMode
-        ? <IEError />
-        : <App /> }
-  </Router>
+	<Router>
+		{window.innerWidth < 768 ? (
+			<MobileError />
+		) : !!document.documentMode ? (
+			<IEError />
+		) : (
+			<App />
+		)}
+	</Router>
 );
 
-ReactDOM.render(<Site />, document.getElementById('root'));
+ReactDOM.render(<Site />, document.getElementById("root"));
 registerServiceWorker();
