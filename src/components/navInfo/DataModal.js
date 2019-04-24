425e3065ec902b87a52a0e304d78128a4590ecc1
DaStrong <danielstrong.ds@gmail.com>
2019-04-24 16:21:35 +0900

    refactored DataModel to use our new state hook

--- src/components/navInfo/DataModal.js
+++ src/components/navInfo/DataModal.js
@@ -1,44 +1,29 @@
-import React, { PureComponent } from 'react'
-import { Modal } from 'semantic-ui-react'
-import LessonsHolder from '../pages/Lessons/LessonsHolder';
-import DataHolder from '../pages/Data/DataHolder';
+import React from "react";
+import { Modal } from "semantic-ui-react";
+import LessonsHolder from "../pages/Lessons/LessonsHolder";
+import DataHolder from "../pages/Data/DataHolder";
+import { useStore } from "../../store";
 
-class DataModal extends PureComponent {
-  
-  render(){
-    const { 
-      sendData, isGameReady, dataModalName, showDataModal, closeModal, vocabulary, expressions
-    } = this.props;
-    const showLessons = dataModalName === 'lessons';
-    const showData = dataModalName === 'data';
-    return (
-      <Modal 
-        basic
-        size='large'
-        className='data-modal'
-        open={showDataModal}
-        onClose={closeModal}
-      >
-        <Modal.Content>
-          <div className='page-container-inner'>
-            {showLessons
-              ? <LessonsHolder
-                  sendData={sendData} 
-                  isGameReady={isGameReady}
-                  fromDataModal={true}
-                />
-              : <DataHolder 
-                  {...this.props}
-                  vocabulary={showData ? [] : vocabulary}
-                  expressions={showData ? [] : expressions}
-                  fromDataModal={true}
-                />
-            }
-          </div>
-        </Modal.Content>
-      </Modal>
-    )
-  }
+export default function DataModal() {
+  const [{ showDataModal, dataModalType }, dispatch] = useStore();
+
+  return (
+    <Modal
+      basic
+      size="large"
+      className="data-modal"
+      open={showDataModal}
+      onClose={() => dispatch({ type: "closeDataModal" })}
+    >
+      <Modal.Content>
+        <div className="page-container-inner">
+          {dataModalType === "lessons" ? (
+            <LessonsHolder fromDataModal={true} />
+          ) : (
+            <DataHolder fromDataModal={true} />
+          )}
+        </div>
+      </Modal.Content>
+    </Modal>
+  );
 }
-
-export default DataModal;
