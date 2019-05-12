693256c1a2fff6c3ef5aa41a1993cfdcb634f8ff
dastrong <danielstrong.ds@gmail.com>
2019-05-12 21:01:02 +0900

    created reusable Confirm box

---
+++ src/components/reusable/ConfirmBox.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Confirm } from "semantic-ui-react";
+import "./ConfirmBox.css";
+
+export default function({ open, onCancel = () => {}, onConfirm = () => {}, ...props }) {
+  const [isOpen, setIsOpen] = useState(open);
+
+  function handleActions(callback) {
+    setIsOpen(false);
+    callback();
+  }
+
+  return (
+    <Confirm
+      className="confirm-box"
+      open={isOpen}
+      onCancel={() => handleActions(onCancel)}
+      onConfirm={() => handleActions(onConfirm)}
+      {...props}
+    />
+  );
+}
