c80cdb6fe78d006859a6193332d8e35a85613add
DaStrong <danielstrong.ds@gmail.com>
2019-05-22 16:28:31 +0900

    updated FAQs

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Accordion } from "semantic-ui-react";
 import PageHeader from "./reusable/PageHeader";
+import TextLink from "./reusable/TextLink";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import "./_Info.css";
 
@@ -28,13 +28,7 @@ const panels = [
       key: "What-is-this-site-content",
       content: (
         <p>
-          For starters, read about us{" "}
-          {
-            <Link to={{ pathname: "/about", state: { pageTransition: "slideUp" } }}>
-              here
-            </Link>
-          }
-          .
+          For starters, read about us <TextLink path="about" text=" here" />.
         </p>
       ),
     },
@@ -47,12 +41,7 @@ const panels = [
       content: (
         <div>
           <p>
-            Head to our{" "}
-            {
-              <Link to={{ pathname: "/contact", state: { pageTransition: "slideUp" } }}>
-                contact
-              </Link>
-            }{" "}
+            Head to our <TextLink path="contact" text=" contact " />
             page.
           </p>
           <p>If you have an urgent question, use the Kakao group chat.</p>
@@ -69,8 +58,8 @@ const panels = [
         <div>
           <p>English public school teachers in South Korea will benefit the most.</p>
           <p>
-            Although any teacher can enter custom data and modify the rules for their
-            classes.
+            Although anyone can enter their own material and modify the gameplay rules to
+            accommodate classes.
           </p>
         </div>
       ),
@@ -81,7 +70,21 @@ const panels = [
     title: "Are there any fees?",
     content: {
       key: "Are-there-any-fees-content",
-      content: <p>Unlike other websites, this one is completely free to use.</p>,
+      content: (
+        <div>
+          <p>Unlike other websites, this one is completely free to use.</p>
+          <p>
+            However, if you would like to support me, you can buy me a coffee{" "}
+            <a
+              href="https://www.buymeacoffee.com/ycqPbFl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here.
+            </a>
+          </p>
+        </div>
+      ),
     },
   },
   {
@@ -105,7 +108,9 @@ const panels = [
       content: (
         <div>
           <p>Yes!</p>
-          <p>There's about 10 more that I'm looking to add.</p>
+          <p>
+            There are a couple other games that I'd like to add finding free time is hard.
+          </p>
         </div>
       ),
     },
@@ -132,11 +137,7 @@ const panels = [
         <div>
           <p>It depends.</p>
           <p>
-            {
-              <Link to={{ pathname: "/contact", state: { pageTransition: "slideUp" } }}>
-                Contact
-              </Link>
-            }{" "}
+            <TextLink path="contact" text="Contact " />
             me to discuss.
           </p>
         </div>
@@ -175,6 +176,88 @@ const panels = [
           <p>
             In my tests, if it's awake it'll load under 2 seconds. The wake up process
             should take under 10 seconds.
+          </p>
+        </div>
+      ),
+    },
+  },
+  {
+    key: "where-did-my-data-go",
+    title: "I lost my data, where did my data go?",
+    content: {
+      key: "where-did-my-data-go-content",
+      content: (
+        <div>
+          <p>
+            This site is build differently than other's you might use and that was on
+            purpose.
+          </p>
+          <p>If your browser tab refreshed the data you were using will be reset.</p>
+          <p>
+            However, if you click the past lessons button on the
+            <TextLink path="" text=" home page " />
+            you can use your data again.
+          </p>
+        </div>
+      ),
+    },
+  },
+  {
+    key: "past-lessons",
+    title: "What's that past lessons button for?",
+    content: {
+      key: "past-lessons-content",
+      content: (
+        <div>
+          <p>By popular request, I've added a way to use your past lessons.</p>
+          <p>Whenever you set data, it will save your data to your browser.</p>
+          <p>
+            If you want to edit one of your lessons, check it, set it, edit it (by opening
+            the left side menu and clicking 'Edit Data'), and set it again.
+          </p>
+          <p>
+            FYI: you can select two or more past lessons to review different lessons too!
+          </p>
+        </div>
+      ),
+    },
+  },
+  {
+    key: "past-lessons-not-clickable",
+    title: "Why can't I click the past lessons button?",
+    content: {
+      key: "past-lessons-not-clickable-content",
+      content: (
+        <div>
+          <p>Simply put, there's no data for this site on the browser you're using.</p>
+          <p>Clearing your browser cache will erase all past lessons.</p>
+          <p>
+            You can <TextLink path="contact" text=" contact " />
+            me, if needed.
+          </p>
+        </div>
+      ),
+    },
+  },
+  {
+    key: "buy-me-a-coffee",
+    title: "Buy you a coffee?",
+    content: {
+      key: "buy-me-a-coffee-content",
+      content: (
+        <div>
+          <p>The site is free to use and will be kept that way.</p>
+          <p>With that said... I like coffee. Coffee is cheap. People can be generous.</p>
+          <p>Any amount is appreciated.</p>
+          <p>
+            You can donate{" "}
+            <a
+              href="https://www.buymeacoffee.com/ycqPbFl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here.
+            </a>
           </p>
         </div>
       ),
