dd59e416df28e51bd70a4bab23c53baddf77e0ce
dastrong <danielstrong.ds@gmail.com>
2019-05-24 13:35:41 +0900

    updates game data if data is edited in game

--- src/components/reusable/WithStoreAndInfo.js
+++ src/components/reusable/WithStoreAndInfo.js
@@ -1,4 +1,4 @@
-import React, { cloneElement } from "react";
+import React, { useState, useEffect, cloneElement } from "react";
 import InfoModal from "../InfoModal";
 import { useStore } from "../../store";
 
@@ -6,6 +6,7 @@ export default function WithStoreAndInfo({ children, gameInfo, path }) {
   const { keyCuts, info } = gameInfo;
   const { attachments, title, images } = info;
 
+  const [wasDataUpdated, setDataUpdateFlag] = useState(false);
   const [store, dispatch] = useStore();
   const {
     vocabulary,
@@ -17,6 +18,15 @@ export default function WithStoreAndInfo({ children, gameInfo, path }) {
     showDataModal,
   } = store;
 
+  useEffect(() => {
+    setDataUpdateFlag(true);
+  }, [vocabulary, expressions]);
+
+  useEffect(() => {
+    if (!wasDataUpdated) return;
+    setDataUpdateFlag(false);
+  }, [wasDataUpdated]);
+
   const childProps = path.includes("/play")
     ? {
         title,
@@ -24,6 +34,7 @@ export default function WithStoreAndInfo({ children, gameInfo, path }) {
         vocabulary,
         expressions,
         colors,
+        dataUpdated: wasDataUpdated,
         isMenuOpen: showSideBar || showDataModal,
       }
     : { title, font, isGameReady, path, image: images.topText };
