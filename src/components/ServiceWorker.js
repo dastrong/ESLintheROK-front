95f81d3a11aafb7381c9d379fa9142d4969d7f3e
Daniel Strong <danielstrong.ds@gmail.com>
2019-05-25 11:36:05 +0900

    revert localhost flag variable back to original for production

--- src/components/ServiceWorker.js
+++ src/components/ServiceWorker.js
@@ -62,7 +62,7 @@ function register() {
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (!isLocalhost) {
+      if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
 
