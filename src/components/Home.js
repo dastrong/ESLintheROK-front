c42ed694ef4cd01692da54b279ce0c88b340060f
Daniel Strong <danielstrong.ds@gmail.com>
2019-05-25 11:48:56 +0900

    testing SW

--- src/components/Home.js
+++ src/components/Home.js
@@ -100,7 +100,7 @@ export default function Home() {
       </Link>
 
       <Button
-        color="blue"
+        color="green"
         size="large"
         disabled={!pastLessons.length}
         onClick={() => dispatch({ type: "togglePastLessons", bool: true })}
