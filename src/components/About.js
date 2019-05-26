a993e91f2aa21b6367cf949cc34ef80c8324c557
Daniel Strong <danielstrong.ds@gmail.com>
2019-05-26 13:05:09 +0900

    updated About

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import PageHeader from "./reusable/PageHeader";
+import TextLink from "./reusable/TextLink";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import "./_Info.css";
 
@@ -19,25 +19,15 @@ export default function About() {
             experience. I'm far superior to my powerpoint counterpart (you won't
             experience any blue screen of death with me either).
           </p>
-          <p>I'm completely free-to-use!</p>
+          <p>And I'm completely free-to-use!</p>
           <p>
-            If you have any questions, head over to my{" "}
-            {
-              <Link
-                to={{
-                  pathname: "/faq",
-                  state: { pageTransition: "slideUp" },
-                }}
-              >
-                FAQs
-              </Link>
-            }
-            .
+            If you have any questions, head over to my
+            <TextLink path="faq" text=" FAQs." />
           </p>
           <h1>The Creator</h1>
+          <p>Hey, I'm Daniel.</p>
           <p>
-            Hey, I'm Daniel. Currently, I work at two public elementary schools for the
-            GOE in Changwon.
+            Currently, I work at two public elementary schools for the GOE in Changwon.
           </p>
           <p>
             For the last year or so, I've been learning how to program and this site is my
@@ -45,22 +35,8 @@ export default function About() {
             produce a site that I was confident could and would be utilized around Korea.
           </p>
           <p>
-            Even though there's already 8 games, there are many still to be added. Let me
-            know here what games you would like to see next.
-          </p>
-          <p>
             If there are any developers out there that want to help improve site features
-            or create new games,{" "}
-            {
-              <Link
-                to={{
-                  pathname: "/contact",
-                  state: { pageTransition: "slideUp" },
-                }}
-              >
-                contact
-              </Link>
-            }{" "}
+            or create new games, <TextLink path="contact" text=" contact " />
             me to discuss.
           </p>
         </div>
