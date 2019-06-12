1110da1c953e619212e8115c11bae98d4c69c7f0
DaStrong <danielstrong.ds@gmail.com>
2019-06-12 13:48:06 +0900

    moved dataUpdated logic into a custom hook

---
+++ src/hooks/useHandleGame.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export default function useHandleGame(handleGame, didUpdate) {
+  // call the handleGame function after the useReducer
+  // from the game has set the initial data as shuffled
+  useEffect(() => {
+    handleGame();
+  }, []);
+
+  // didUpdate comes from useData
+  // if data was edited, reset the game using that new data
+  useEffect(() => {
+    if (!didUpdate) return;
+    handleGame();
+  }, [didUpdate]);
+}
--- src/hooks/useUpdateData.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export default function useUpdateData(dataUpdated, handleGameRef) {
-  useEffect(() => {
-    if (!dataUpdated) return;
-    handleGameRef();
-  }, [handleGameRef, dataUpdated]);
-}
