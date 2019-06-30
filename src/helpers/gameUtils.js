75a394428f03ba649b9f7bda57e069d3a63ba88e
Daniel Strong <danielstrong.ds@gmail.com>
2019-06-30 20:15:44 +0900

    separated some functions from phase2helpers

---
+++ src/helpers/gameUtils.js
@@ -0,0 +1,64 @@
+import shuffle from "lodash/shuffle";
+
+const uniqueLengthErr = `Cannot make a unique array, if the total length is lower than the max number`;
+const minMaxErr = `Min cannot be higher than the max`;
+
+export function getRandomNum(length) {
+  return Math.floor(Math.random() * length);
+}
+
+// used to get the next set of data for our gameData arrays
+export function nextRoundData(data, count, isVocab, vocabulary, expressions) {
+  const shuffledData = () => (isVocab ? shuffle(vocabulary) : shuffle(expressions));
+  // if there isn't enough data for the next round, refresh all data and shuffle it
+  const newData = count <= data.length ? [...data] : shuffledData();
+  // get 'count' amount of data for the next round
+  const roundD = newData.splice(0, count);
+  // returns this rounds data and the next rounds data
+  return [roundD, newData];
+}
+
+// returns a random linear gradient string
+export function getRandoGrad() {
+  const deg = getRandomNum(360);
+  const st = getHSL();
+  const nd = getHSL();
+  const rd = getHSL();
+  return `linear-gradient(${deg}deg, ${st}, ${nd}, ${rd})`;
+}
+
+// creates an array of random numbers within the parameters given
+// can be unique if needed
+export function arrOfRandoNum(min, max, num, unique = false) {
+  if (min > max) throw new Error(`error in arrOfRandoNum. ${minMaxErr}`);
+  if (unique && max < num) throw new Error(`error in arrOfRandoNum. ${uniqueLengthErr}`);
+  let arr = [];
+  while (arr.length < num) {
+    const randoNum = getRandomNum(max - min) + min;
+    if (unique) {
+      if (arr.indexOf(randoNum) === -1) {
+        arr.push(randoNum);
+      }
+    } else {
+      arr.push(randoNum);
+    }
+  }
+  return arr;
+}
+
+// used for multi data games
+// bails out of the dispatch to avoid unnecessary rerenders
+// extras is an object of variables that you might need updated when updating isVocab
+export function changeIsVocab(isVocab, state, extras) {
+  if (isVocab === state.isVocab) return state;
+  return { ...state, isVocab, ...extras };
+}
+
+// returns a random HSL string
+// not exported since it's only used in getRandoGrad above currently
+function getHSL() {
+  const hue = getRandomNum(360);
+  const sat = getRandomNum(20) + 80;
+  const lig = getRandomNum(10) + 60;
+  return `hsl(${hue}, ${sat}%, ${lig}%)`;
+}
