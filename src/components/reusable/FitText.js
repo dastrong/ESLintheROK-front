30f11f18bbcb2958eb4349d38c48bd962320aa9f
dastrong <danielstrong.ds@gmail.com>
2019-07-13 18:22:11 +0900

    updated FitText component and styling

--- src/components/reusable/FitText.js
+++ src/components/reusable/FitText.js
@@ -2,14 +2,11 @@ import React, { forwardRef } from "react";
 import classNames from "classnames";
 import "./FitText.css";
 
-// can pass additional style through a style props
-// for certain games like Kimchi where we need to cover the whole screen
-// a width style should be used to break/wrap the text
-// a className may be passed down for further styling
-export default forwardRef(({ text, style, cx }, ref) => {
+// a className (cx) may be passed down for further styling
+export default forwardRef(({ text, cx }, ref) => {
   const className = classNames("fittext", { [cx]: cx });
   return (
-    <span className={className} style={style} ref={ref}>
+    <span className={className} ref={ref}>
       {text}
     </span>
   );
