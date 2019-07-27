ad85e048b68f36cbfd0ad113eb9637e73cbfb832
dastrong <danielstrong.ds@gmail.com>
2019-07-27 19:23:09 +0900

    improved Card CSS and added class to animate Card out of view

--- src/components/reusable/Card.js
+++ src/components/reusable/Card.js
@@ -6,8 +6,17 @@ import "./Card.css";
 // returns a div containing two divs that are back to back
 // need to specify a width/height in your CSS
 export default forwardRef((props, ref) => {
-  const { textFront, textBack, colorFront, colorBack, handleClick, id, flipped } = props;
-  const cx = classnames("flipper-card", { flipped });
+  const {
+    textFront,
+    textBack,
+    colorFront,
+    colorBack,
+    handleClick,
+    id,
+    flipMe,
+    slideMe,
+  } = props;
+  const cx = classnames("flipper-card", { flipMe, slideMe });
   return (
     <div className={cx} id={id} onClick={handleClick}>
       <div className="flipper-card front" style={{ backgroundColor: colorFront }}>
