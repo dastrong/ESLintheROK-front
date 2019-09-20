dbb7b620684bcbb5682a41f5a9822e92722898da
dastrong <danielstrong.ds@gmail.com>
2019-09-20 21:51:14 +0900

    added path aliases

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,14 @@
 const CracoWorkboxPlugin = require("craco-workbox");
+const path = require("path");
+
+const resolve = arg => path.resolve(__dirname, arg);
 
 module.exports = {
+  webpack: {
+    alias: {
+      "@Game": resolve("src/components/games/"),
+    },
+  },
   plugins: [
     {
       plugin: CracoWorkboxPlugin,
