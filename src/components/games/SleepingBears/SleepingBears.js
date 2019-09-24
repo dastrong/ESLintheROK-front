8c1fb822d49eb52c3e81178d3846c186c4171653
DaStrong <danielstrong.ds@gmail.com>
2019-09-24 16:25:40 +0900

    WIP - need some animation and a larger than 4 words comparison function

--- src/components/games/SleepingBears/SleepingBears.js
+++ src/components/games/SleepingBears/SleepingBears.js
@@ -1,8 +1,6 @@
 import React, { useCallback, forwardRef } from "react";
 import shuffle from "lodash/shuffle";
-// import classNames from "classnames";
-// import {useSpring} from "react-spring"
-// import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { useSpring } from "react-spring";
 import useData from "hooks/useData";
 import useKeys from "hooks/useKeys";
 import useScroll from "hooks/useScroll";
@@ -11,12 +9,7 @@ import useFirstRun from "hooks/useFirstRun";
 import useHandleGame from "hooks/useHandleGame";
 import useDocumentTitle from "hooks/useDocumentTitle";
 import { googleEvent } from "helpers/ga";
-import {
-  nextRoundData,
-  arrOfRandoNum,
-  changeIsVocab,
-  getRandoNum,
-} from "helpers/gameUtils";
+import { nextRoundData, changeIsVocab, getRandoNum } from "helpers/gameUtils";
 import FitText from "@Reusable/FitText";
 import "./SleepingBears.css";
 
@@ -47,7 +40,7 @@ function reducer(state, action) {
 
 export default function SleepingBears(props) {
   // PROPS
-  const { title, isMenuOpen, font, vocabulary, expressions, colors } = props;
+  const { title, isMenuOpen, font, vocabulary, expressions } = props;
   useDocumentTitle(`Playing - ${title} - ESL in the ROK`);
   const isFirstRun = useFirstRun();
 
@@ -56,12 +49,17 @@ export default function SleepingBears(props) {
   const { data, isVocab, gameData } = state;
   const [refs] = useFitText(gameData.length, gameData, font, false);
 
+  // SPRINGS
+  // const springs = useSprings(gameData);
+
   // HANDLE GAME
   const handleGame = useCallback(() => {
     console.log("new round");
     googleEvent(title);
-    const [cur, nex] = nextRoundData(data, 4, isVocab, vocabulary, expressions);
-    dispatch({ type: "New_Round", data: nex, gameData: cur });
+    const [[cur], nex] = nextRoundData(data, 1, isVocab, vocabulary, expressions);
+    const gameData = isVocab ? __splitVocab(cur) : __splitExpression(cur);
+    const shuffledGD = shuffle(gameData);
+    dispatch({ type: "New_Round", data: nex, gameData: shuffledGD });
   }, [data, isVocab]);
   useHandleGame(handleGame, didUpdate);
 
@@ -102,15 +100,81 @@ export default function SleepingBears(props) {
 }
 
 // INNER COMPONENTS HERE
-const Card = forwardRef(({ text, i }, ref) => (
-  <div className="sleeping-card">
-    <div className="sleeping-card-front">
-      <img src={bearImgs[i]} alt={"bear-" + (i + 1)} />
+const Card = forwardRef(({ text, i }, ref) => {
+  // const spring = useSpring({ opacity: showText ? 1 : 0 });
+
+  return (
+    <div className="sleeping-card">
+      <div className="sleeping-card-front">
+        <img src={bearImgs[i]} alt={"bear-" + (i + 1)} />
+      </div>
+      <div className="sleeping-card-back">
+        <FitText ref={ref} text={text} />
+      </div>
     </div>
-    <div className="sleeping-card-back">
-      <FitText ref={ref} text={text} />
-    </div>
-  </div>
-));
+  );
+});
 
 // OTHER FUNCTIONS HERE
+function __splitVocab(text) {
+  if (text.length > 4) {
+    const splitSpots = __getSplitSpots(text.length);
+    const splitText = text.split("");
+    return splitSpots.map(num => splitText.splice(0, num));
+  } else if (text.length < 4) {
+    return Array.from(Array(4), (_, i) => text[i] || "");
+  } else {
+    return text.split("");
+  }
+}
+
+function __splitExpression(text) {
+  const split = text.split(" ");
+
+  if (split.length === 1) return __splitVocab(text);
+
+  if (split.length === 2) {
+    const halved = split.map(string => __halvedWord(string));
+    return halved.reduce((acc, cVal) => [...acc, ...cVal], []);
+  }
+
+  if (split.length === 3) {
+    const longestWord = split.reduce((acc, cVal) => (acc > cVal ? acc : cVal), "");
+    const otherWords = split.filter(word => word !== longestWord);
+    const splitWord = __halvedWord(longestWord);
+    return [...otherWords, ...splitWord];
+  }
+
+  if (split.length === 4) return split;
+
+  if (split.length > 4) {
+    // much more complicated
+    const indexOfSpaces = text
+      .split("")
+      .reduce((acc, cVal, i) => (cVal === " " ? [...acc, i] : acc), []);
+    const allIndexes = indexOfSpaces.push(text.length);
+    console.log(allIndexes);
+  }
+}
+
+// used to evenly split a string, when the string is longer than 4
+// returns an array of shuffled numbers where the string should be sliced
+function __getSplitSpots(strLength) {
+  const boxCount = 4;
+  let length = strLength;
+  const splitSpots = Array.from(Array(boxCount), (_, i) => {
+    const splitSpot = Math.ceil(length / (boxCount - i));
+    length -= splitSpot;
+    return splitSpot;
+  });
+  return shuffle(splitSpots);
+}
+
+// split a word into equal parts
+// returns an array of both halves of the string
+function __halvedWord(word) {
+  const slicePos = Math.floor(word.length / 2);
+  const start = word.substring(0, slicePos);
+  const end = word.substring(slicePos);
+  return [start, end];
+}
