36f6ad637d84f7885dbe4801bbe1d75abda28baa
dastrong <danielstrong.ds@gmail.com>
2019-09-29 12:18:56 +0900

    added verifyData function to gameUtils

--- src/helpers/gameUtils.js
+++ src/helpers/gameUtils.js
@@ -8,6 +8,26 @@ export function getRandoNum(max, min = 0) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+// skip values with _ (indicating blank words) or ...
+export function verifyGameData(data, isVocab, vocabulary, expressions) {
+  let stateData = data;
+  let gameData = "";
+  let nextData = [];
+  for (let i = 0; i <= data.length; i++) {
+    const [[cur], nex] = nextRoundData(stateData, 1, isVocab, vocabulary, expressions);
+    const isAcceptable = !(cur.includes("_") || cur.includes("..."));
+    if (isAcceptable) {
+      gameData = cur;
+      nextData = nex;
+      break;
+    } else {
+      stateData = nex;
+      nextData = nex;
+    }
+  }
+  return [gameData, nextData];
+}
+
 // used to get the next set of data for our gameData arrays
 export function nextRoundData(data, count, isVocab, vocabulary, expressions) {
   const shuffledData = () => shuffle(isVocab ? vocabulary : expressions);
