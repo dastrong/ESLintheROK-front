5d13a2f7896402d1477b6228c92d0115f1d1a84b
DaStrong <danielstrong.ds@gmail.com>
2021-04-21 16:29:37 +0900

    show the components on a page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Button from 'components/Button';
+import Checkbox from 'components/Checkbox';
+import Input from 'components/Input';
+import Range from 'components/Range';
+import TextArea from 'components/TextArea';
 
 export default function AboutPage() {
   return (
     <div>
       <p>About Page</p>
+
+      <Button textColor="white" bgColor="var(--color-blue-400)" />
+      <Button textColor="white" bgColor="yellow" />
+      <Checkbox />
+      <Input />
+      <Range min={0} max={100} />
+      <TextArea defaultValue="Hey" />
 
       <style jsx>{``}</style>
     </div>
