0b5e0e0078768500ce8b9e9e2ae72c40ee11e22d
DaStrong <danielstrong.ds@gmail.com>
2021-04-21 16:28:39 +0900

    init button styling

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,12 +1,37 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames';
+import { VarColorTypes } from 'utils/theme';
 
-export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+type Button = {
+  textColor: string;
+  bgColor: string;
+  icon?: boolean;
+  text?: string;
+  children?: React.ReactNode;
+  spinner?: boolean;
+};
+
+export default function Button({
+  textColor,
+  bgColor,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement> & {
+  textColor: string;
+  bgColor: VarColorTypes | 'green' | 'yellow';
+}) {
+  // apply global button styles
+  const cx = classNames('btn', props.className);
+
   return (
-    <button {...props}>
+    <button {...props} className={cx}>
       This is a Button
       <style jsx>{`
         button {
-          /*  */
+          margin: 0;
+          padding: 0.75rem 1.2rem;
+          border-radius: 5px;
+          color: ${textColor};
+          background-color: ${bgColor};
         }
       `}</style>
     </button>
