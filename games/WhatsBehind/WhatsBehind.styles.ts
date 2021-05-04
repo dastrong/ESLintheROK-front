f1aa714f2465f2e4d7d1ac820d9ecb0e59a25989
DaStrong <danielstrong.ds@gmail.com>
2021-05-04 16:19:47 +0900

    corrected font to font-family

--- games/WhatsBehind/WhatsBehind.styles.ts
+++ games/WhatsBehind/WhatsBehind.styles.ts
@@ -2,7 +2,7 @@ import { css } from 'styled-jsx/css';
 
 export const getContainerCSS = (fontFamily: string) => css.resolve`
    {
-    font: ${fontFamily};
+    font-family: ${fontFamily};
     overflow: hidden;
     height: 100vh;
     display: flex;
--- games/WordLotto/WordLotto.styles.ts
+++ games/WordLotto/WordLotto.styles.ts
@@ -2,7 +2,7 @@ import { css } from 'styled-jsx/css';
 
 export const getContainerCSS = (fontFamily: string) => css.resolve`
    {
-    font: ${fontFamily};
+    font-family: ${fontFamily};
     /* rest of your container styles here */
   }
 `;
