4d736464a01f7b818f1f1ac1593e043647f996cf
DaStrong <danielstrong.ds@gmail.com>
2021-05-12 16:24:24 +0900

    updated ChaseTheVocab game

--- games/ChaseTheVocab/state_types.ts
+++ games/ChaseTheVocab/state_types.ts
@@ -1,13 +1,36 @@
 import { Dispatch } from 'react';
 
+export type GameData = {
+  text: string;
+  id: number;
+}[];
+
 export type State = {
   data: string[];
-  isVocab: boolean; // delete if there is only one data source
+  gameData: GameData;
+  round: number;
+  color: number;
+  clickedIDs: number[];
+  shuffDuration: number;
+  shuffBuffer: number;
+  shuffRounds: number;
+  isAnimating: boolean;
+  isShuffleDone: boolean;
 };
 
 export type Action =
   | { type: 'Set_Data'; data: string[] }
-  | { type: 'Change_isVocab'; isVocab: boolean } // delete if there is only one data source
-  | { type: 'New_Round' }; // add other variables here
+  | { type: 'New_Round'; data: string[]; gameData: GameData }
+  | { type: 'Add_Click_ID'; id: number }
+  | {
+      type: 'Change_Settings';
+      shuffBuffer: number;
+      shuffDuration: number;
+      shuffRounds: number;
+    }
+  | { type: 'Change_Color'; color: number }
+  | { type: 'Start_Animating' }
+  | { type: 'Shuffle'; gameData: GameData }
+  | { type: 'Shuffle_Stop' };
 
 export type GameStore = [State, Dispatch<Action>, boolean];
