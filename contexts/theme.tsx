2d2717f16243f1d32c4c17b4c1ff65b35c21aaf2
Daniel Strong <danielstrong.ds@gmail.com>
2021-05-30 08:52:01 +0900

    created/added a ThemeProvider

---
+++ contexts/theme.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+type ThemeState = {
+  font: string;
+  colors: {
+    blue: string;
+    red: string;
+    gray: string;
+  };
+};
+
+type ThemeAction = { type: 'ChangeFont'; font: string };
+
+const theme: ThemeState = {
+  font: '',
+  colors: {
+    blue: '#0047a0',
+    red: '#cd2e3a',
+    gray: '#d0cbcd',
+  },
+};
+
+const reducer = (state: ThemeState, action: ThemeAction) => {
+  switch (action.type) {
+    case 'ChangeFont':
+      return { ...state, font: action.font };
+    default:
+      return state;
+  }
+};
+
+const ThemeContext = createContext(
+  {} as { theme: ThemeState; themeDispatch: React.Dispatch<ThemeAction> }
+);
+
+export const ThemeProvider = ({ children }) => {
+  const [state, themeDispatch] = useReducer(reducer, theme);
+
+  return (
+    <ThemeContext.Provider value={{ theme: state, themeDispatch }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useStore = () => useContext(ThemeContext);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,89 +5,92 @@ import Sidebar from '../components/Sidebar';
 import Layout from '../components/Layout';
 import { DataModals } from 'components/Modal(s)';
 import { StoreProvider } from '../contexts/store';
+import { ThemeProvider } from 'contexts/theme';
 import { globalCSSVariables } from 'utils/theme';
 import 'normalize.css';
 import '@fontsource/lato';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StoreProvider>
-      <IconContext.Provider value={{ style: { verticalAlign: 'bottom' } }}>
-        <Sidebar />
-        <DataModals />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </IconContext.Provider>
+    <ThemeProvider>
+      <StoreProvider>
+        <IconContext.Provider value={{ style: { verticalAlign: 'bottom' } }}>
+          <Sidebar />
+          <DataModals />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </IconContext.Provider>
 
-      {/* CSS Variables */}
-      <style jsx global>
-        {globalCSSVariables}
-      </style>
+        {/* CSS Variables */}
+        <style jsx global>
+          {globalCSSVariables}
+        </style>
 
-      {/* BUTTON DEFAULTS */}
-      <style jsx global>{`
-        button {
-          cursor: pointer;
-          outline: 0;
-          border: none;
-          text-decoration: none;
-          user-select: none;
-          line-height: 1;
-        }
-      `}</style>
+        {/* BUTTON DEFAULTS */}
+        <style jsx global>{`
+          button {
+            cursor: pointer;
+            outline: 0;
+            border: none;
+            text-decoration: none;
+            user-select: none;
+            line-height: 1;
+          }
+        `}</style>
 
-      {/* THE REST */}
-      <style jsx global>{`
-        body {
-          font-family: Lato, Helvetica Neue, Arial, Helvetica, sans-serif;
-          overflow: hidden;
-        }
+        {/* THE REST */}
+        <style jsx global>{`
+          body {
+            font-family: Lato, Helvetica Neue, Arial, Helvetica, sans-serif;
+            overflow: hidden;
+          }
 
-        /* SCROLLBAR */
-        ::-webkit-scrollbar {
-          -webkit-appearance: none;
-          width: 10px;
-          height: 10px;
-        }
+          /* SCROLLBAR */
+          ::-webkit-scrollbar {
+            -webkit-appearance: none;
+            width: 10px;
+            height: 10px;
+          }
 
-        ::-webkit-scrollbar-track {
-          background: rgba(0, 0, 0, 0.1);
-          border-radius: 0;
-        }
+          ::-webkit-scrollbar-track {
+            background: rgba(0, 0, 0, 0.1);
+            border-radius: 0;
+          }
 
-        ::-webkit-scrollbar-thumb {
-          cursor: pointer;
-          border-radius: 5px;
-          background: rgba(0, 0, 0, 0.25);
-          -webkit-transition: color 0.2s ease;
-          transition: color 0.2s ease;
-        }
+          ::-webkit-scrollbar-thumb {
+            cursor: pointer;
+            border-radius: 5px;
+            background: rgba(0, 0, 0, 0.25);
+            -webkit-transition: color 0.2s ease;
+            transition: color 0.2s ease;
+          }
 
-        ::-webkit-scrollbar-thumb:window-inactive {
-          background: rgba(0, 0, 0, 0.15);
-        }
+          ::-webkit-scrollbar-thumb:window-inactive {
+            background: rgba(0, 0, 0, 0.15);
+          }
 
-        ::-webkit-scrollbar-thumb:hover {
-          background: rgba(128, 135, 139, 0.8);
-        }
+          ::-webkit-scrollbar-thumb:hover {
+            background: rgba(128, 135, 139, 0.8);
+          }
 
-        ::-webkit-scrollbar-track {
-          background: rgba(255, 255, 255, 0.1);
-        }
+          ::-webkit-scrollbar-track {
+            background: rgba(255, 255, 255, 0.1);
+          }
 
-        ::-webkit-scrollbar-thumb {
-          background: rgba(255, 255, 255, 0.25);
-        }
+          ::-webkit-scrollbar-thumb {
+            background: rgba(255, 255, 255, 0.25);
+          }
 
-        ::-webkit-scrollbar-thumb:window-inactive {
-          background: rgba(255, 255, 255, 0.15);
-        }
+          ::-webkit-scrollbar-thumb:window-inactive {
+            background: rgba(255, 255, 255, 0.15);
+          }
 
-        ::-webkit-scrollbar-thumb:hover {
-          background: rgba(255, 255, 255, 0.35);
-        }
-      `}</style>
-    </StoreProvider>
+          ::-webkit-scrollbar-thumb:hover {
+            background: rgba(255, 255, 255, 0.35);
+          }
+        `}</style>
+      </StoreProvider>
+    </ThemeProvider>
   );
 }
