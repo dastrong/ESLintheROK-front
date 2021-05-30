9bd5bceee178e933a49809e226a107be7ab53853
Daniel Strong <danielstrong.ds@gmail.com>
2021-05-30 20:01:54 +0900

    initial input and textarea styles

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -7,7 +7,23 @@ export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
 
       <style jsx>{`
         input {
-          /*  */
+          margin: 0;
+          outline: 0;
+          line-height: 1rem;
+          padding: 0.7rem 1rem;
+          font-size: 1rem;
+          background: #fff;
+          border: 1px solid rgba(34, 36, 38, 0.15);
+          color: rgba(0, 0, 0, 0.87);
+          border-radius: 0.2rem;
+          box-shadow: inset 0 0 0 0 transparent;
+          transition: color 0.1s ease, border-color 0.1s ease;
+        }
+
+        input:focus {
+          color: rgba(0, 0, 0, 0.95);
+          border-color: #85b7d9;
+          box-shadow: inset 0 0 0 0 rgb(34 36 38 / 35%);
         }
       `}</style>
     </>
