ba12b5365a4c4f471b35a2e3dbd8e662bb0e127b
DaStrong <danielstrong.ds@gmail.com>
2021-07-14 15:58:34 +0900

    updated Contact page styling (QR codes might need changing)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,9 @@
 import React, { FormEvent, useState } from 'react';
 import Button from 'components/Button';
 import { InputCSS } from 'components/Styles';
+import QrKakao from 'components/Svgs/qr_kakao.svg';
+import QrKorshare from 'components/Svgs/qr_korshare.svg';
+import QrWaygook from 'components/Svgs/qr_waygook.svg';
 
 export default function ContactPage() {
   const [name, setName] = useState('');
@@ -23,7 +26,6 @@ export default function ContactPage() {
   return (
     <div>
       <h1 className="heading">Contact</h1>
-
       <div className="sub-heading">
         <p>
           I’d love to hear from you. Contact me directly or join an existing
@@ -52,6 +54,7 @@ export default function ContactPage() {
             />
             <textarea
               required
+              rows={5}
               className={InputCSS.className}
               placeholder="Your question or message here"
               value={content}
@@ -68,7 +71,57 @@ export default function ContactPage() {
         </div>
         <div className="contact_panel interactive">
           <h3>Interactive</h3>
-          <p>Click or scan a code below to join a conversation</p>
+          <p>Click/Scan a code below to join a conversation</p>
+          <div className="interactive_options">
+            {/* KAKAO */}
+            <div className="interactive_option">
+              <div>
+                <h5>KakaoTalk</h5>
+                <p>
+                  Join our chat room to get a short, quick question answered.
+                </p>
+              </div>
+              <a href="https://open.kakao.com/o/giDzG30?rt=Q" target="_blank">
+                <QrKakao
+                  style={{ fill: 'purple', height: 110, display: 'block' }}
+                />
+              </a>
+            </div>
+
+            {/* KORSHARE */}
+            <div className="interactive_option">
+              <div>
+                <h5>KorShare.org</h5>
+                <p>
+                  Newish forum for ESL teachers to share materials and converse.
+                </p>
+              </div>
+              <a
+                href="http://korshare.org/index.php?showtopic=2261"
+                target="_blank"
+              >
+                <QrKorshare
+                  style={{ fill: '#1060e2', height: 110, display: 'block' }}
+                />
+              </a>
+            </div>
+
+            {/* WAYGOOK */}
+            <div className="interactive_option">
+              <div>
+                <h5>Waygook.org</h5>
+                <p>The original ESL forum for ESL teachers in South Korea.</p>
+              </div>
+              <a
+                href="https://www.waygook.org/index.php?topic=114421.0"
+                target="_blank"
+              >
+                <QrWaygook
+                  style={{ fill: 'green', height: 110, display: 'block' }}
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -76,7 +129,7 @@ export default function ContactPage() {
 
       <style jsx>{`
         .heading {
-          margin: 2rem auto 1.25rem;
+          margin: 0rem auto 1.25rem;
           text-align: center;
           color: #414141;
           font-size: 3.5rem;
@@ -103,7 +156,7 @@ export default function ContactPage() {
         .contact_panel {
           position: relative;
           box-sizing: border-box;
-          width: 49%;
+          width: 47%;
           background-color: var(--bgColor);
           padding: var(--padding);
           padding-top: calc(var(--padding) + var(--radius));
@@ -128,17 +181,17 @@ export default function ContactPage() {
         }
 
         .email {
-          --bgColor: rgba(57, 184, 255, 0.1);
+          --bgColor: rgb(184, 226, 250);
           --accentColor: rgba(57, 184, 255, 1);
         }
 
         .interactive {
-          --bgColor: rgba(181, 15, 239, 0.1);
+          --bgColor: rgb(237, 203, 250);
           --accentColor: rgba(181, 15, 239, 1);
         }
 
         .contact_panel h3 {
-          font-size: 1.5rem;
+          font-size: 1.8rem;
           margin: 0 auto 0.75rem;
         }
 
@@ -147,6 +200,55 @@ export default function ContactPage() {
           margin: 0 auto 1rem;
           color: #5a5c62;
           text-align: center;
+        }
+
+        form {
+          width: 100%;
+          margin: 1rem auto 0;
+        }
+
+        input,
+        textarea {
+          margin-bottom: 1.5rem;
+        }
+
+        .interactive_options {
+          margin: 1rem 0 0;
+        }
+
+        .interactive_options h5 {
+          margin: 0 0 1rem;
+          font-size: 1.4rem;
+        }
+
+        .interactive_option {
+          display: flex;
+          justify-content: space-evenly;
+          align-items: center;
+          text-align: left;
+          margin: 1.25rem auto;
+        }
+
+        .interactive_option:first-child {
+          margin-top: 0;
+        }
+
+        .interactive_option:last-child {
+          margin-bottom: 0;
+        }
+
+        .interactive_option:nth-child(even) {
+          flex-direction: row-reverse;
+        }
+
+        .interactive_option p {
+          margin-bottom: 0;
+          text-align: left;
+          font-size: 1rem;
+        }
+
+        .interactive_option div {
+          width: 50%;
         }
       `}</style>
     </div>
