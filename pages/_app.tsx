84f470dbd613367bdc3856f85bd6ef312d632ed1
Daniel Strong <danielstrong.ds@gmail.com>
2021-07-18 22:44:55 +0900

    added a global swr provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 import { IconContext } from 'react-icons/lib';
+
 import Layout from '../components/Layout';
 import { DataModals } from 'components/Modal(s)';
 import { StoreProvider } from '../contexts/store';
@@ -11,74 +13,81 @@ const navWaveHeightToWidthRatio = 192 / 2560;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <StoreProvider>
-        <IconContext.Provider value={{ style: { verticalAlign: 'bottom' } }}>
-          <DataModals />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </IconContext.Provider>
+    <SWRConfig
+      value={{
+        revalidateOnFocus: false,
+        fetcher: (url: string) =>
+          fetch('http://localhost:4000/api' + url).then(r => r.json()),
+      }}
+    >
+      <ThemeProvider>
+        <StoreProvider>
+          <IconContext.Provider value={{ style: { verticalAlign: 'bottom' } }}>
+            <DataModals />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </IconContext.Provider>
 
-        {/* BUTTON DEFAULTS */}
-        <style jsx global>{`
-          button {
-            cursor: pointer;
-            outline: 0;
-            border: none;
-            text-decoration: none;
-            user-select: none;
-            line-height: 1;
-          }
-        `}</style>
+          {/* BUTTON DEFAULTS */}
+          <style jsx global>{`
+            button {
+              cursor: pointer;
+              outline: 0;
+              border: none;
+              text-decoration: none;
+              user-select: none;
+              line-height: 1;
+            }
+          `}</style>
 
-        {/* THE REST */}
-        <style jsx global>{`
-          body {
-            font-family: 'Comic Neue', cursive;
-            overflow-x: hidden;
-            background-color: var(--siteBgColor);
-            --siteBgColor: #cdeeff;
-            --navHeight: calc(100vw * ${navWaveHeightToWidthRatio});
-            --footerHeight: 300px;
-          }
+          {/* THE REST */}
+          <style jsx global>{`
+            body {
+              font-family: 'Comic Neue', cursive;
+              overflow-x: hidden;
+              background-color: var(--siteBgColor);
+              --siteBgColor: #cdeeff;
+              --navHeight: calc(100vw * ${navWaveHeightToWidthRatio});
+              --footerHeight: 300px;
+            }
 
-          *,
-          *:before,
-          *:after {
-            box-sizing: border-box;
-          }
+            *,
+            *:before,
+            *:after {
+              box-sizing: border-box;
+            }
 
-          /* SCROLLBAR */
-          ::-webkit-scrollbar {
-            -webkit-appearance: none;
-            width: 10px;
-            height: 10px;
-          }
+            /* SCROLLBAR */
+            ::-webkit-scrollbar {
+              -webkit-appearance: none;
+              width: 10px;
+              height: 10px;
+            }
 
-          ::-webkit-scrollbar-track {
-            background: rgba(0, 0, 0, 0.1);
-            border-radius: 0;
-          }
+            ::-webkit-scrollbar-track {
+              background: rgba(0, 0, 0, 0.1);
+              border-radius: 0;
+            }
 
-          ::-webkit-scrollbar-thumb {
-            cursor: pointer;
-            border-radius: 5px;
-            background: rgba(0, 0, 0, 0.25);
-            -webkit-transition: color 0.2s ease;
-            transition: color 0.2s ease;
-          }
+            ::-webkit-scrollbar-thumb {
+              cursor: pointer;
+              border-radius: 5px;
+              background: rgba(0, 0, 0, 0.25);
+              -webkit-transition: color 0.2s ease;
+              transition: color 0.2s ease;
+            }
 
-          ::-webkit-scrollbar-thumb:window-inactive {
-            background: rgba(0, 0, 0, 0.15);
-          }
+            ::-webkit-scrollbar-thumb:window-inactive {
+              background: rgba(0, 0, 0, 0.15);
+            }
 
-          ::-webkit-scrollbar-thumb:hover {
-            background: rgba(128, 135, 139, 0.8);
-          }
+            ::-webkit-scrollbar-thumb:hover {
+              background: rgba(128, 135, 139, 0.8);
+            }
 
-           {
-            /* ::-webkit-scrollbar-track {
+             {
+              /* ::-webkit-scrollbar-track {
             background: rgba(255, 255, 255, 0.1);
           }
 
@@ -93,9 +102,10 @@ export default function App({ Component, pageProps }: AppProps) {
           ::-webkit-scrollbar-thumb:hover {
             background: rgba(255, 255, 255, 0.35);
           } */
-          }
-        `}</style>
-      </StoreProvider>
-    </ThemeProvider>
+            }
+          `}</style>
+        </StoreProvider>
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
