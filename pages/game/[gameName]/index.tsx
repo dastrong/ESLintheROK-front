48365cde338024a0095a06268b48cde432091609
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-22 18:04:04 +0900

    corrected query issue again and added SeoWrapper

--- pages/game/[gameName]/index.tsx
+++ pages/game/[gameName]/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GetStaticPaths, InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
-import router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import { FaArrowLeft, FaArrowRight, FaPlay } from 'react-icons/fa';
 
+import SeoWrapper from 'components/SeoWrapper';
 import Button from 'components/Button';
 import Block from 'components/Block';
 import Image from 'components/Image';
@@ -14,7 +15,7 @@ import { PageHeading } from 'components/PageHeadings';
 import { convertCaseSnakeToPascal } from 'utils/convertCaseSnakeToPascal';
 import { getSingleGameConfig } from 'utils/getSingleGameConfig';
 import { getAllGameConfigs } from 'utils/getAllGameConfigs';
-import { getGameImgUrl } from 'utils/getGameImgUrl';
+import { getGameImgUrl, getGameOgImgUrl } from 'utils/getGameImgUrl';
 import { useStore } from 'contexts/store';
 
 const GameInstructionsModal = dynamic(
@@ -31,136 +32,167 @@ const gameNotes = [
 
 export default function GameHomePage({
   title,
+  description,
   image,
   instructions,
   warnings,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { asPath, query } = useRouter();
+  const { asPath, query, push } = useRouter();
   const { isDataReady } = useStore();
 
   const imgUrl = getGameImgUrl(title);
 
+  // SEO
+  const seoTitle = `${title} Game`;
+  const imgWidth = 1200;
+  const ogImgUrl = getGameOgImgUrl(title, `/c_scale,w_${imgWidth}`);
+
   const openInstructionModal = (version: 'teacher' | 'student') => {
-    router.push(`?instructions=${version}&language=english`, undefined, {
-      shallow: true,
-    });
+    push(
+      {
+        query: {
+          ...query,
+          instructions: version,
+          language: 'english',
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
   };
 
   return (
-    <div className="container">
-      <PageHeading>{title}</PageHeading>
+    <SeoWrapper
+      title={seoTitle}
+      description={description}
+      openGraph={{
+        images: [
+          {
+            url: ogImgUrl,
+            width: imgWidth,
+            height: Math.floor((imgWidth / image.width) * image.height),
+            alt: title,
+          },
+        ],
+      }}
+      twitter={{ cardType: 'summary_large_image' }}
+    >
+      <div className="container">
+        <PageHeading>{title}</PageHeading>
 
-      <div className="button_container">
-        <Button
-          rounded
-          size="xl"
-          color="#565656"
-          bgColor="#F8F8F8"
-          text="Teacher Instructions"
-          Icon={FaArrowLeft}
-          iconPosition="left"
-          onClick={() => openInstructionModal('teacher')}
-        />
+        <div className="button_container">
+          <Button
+            rounded
+            size="xl"
+            color="#565656"
+            bgColor="#F8F8F8"
+            text="Teacher Instructions"
+            Icon={FaArrowLeft}
+            iconPosition="left"
+            onClick={() => openInstructionModal('teacher')}
+          />
 
-        <Popup
-          hideTooltip={isDataReady}
-          content="Please choose/create some lesson data first"
-          owner={
-            <div style={{ margin: '0 1rem', display: 'inline-block' }}>
-              <Link href={`${asPath}/play`} passHref>
-                <Button
-                  rounded
-                  size="xl"
-                  as="a"
-                  color="#565656"
-                  bgColor="#97D492"
-                  text="Play"
-                  Icon={FaPlay}
-                  iconPosition="right"
-                  disabled={!isDataReady}
-                />
-              </Link>
-            </div>
-          }
-        />
+          <Popup
+            hideTooltip={isDataReady}
+            content="Please choose/create some lesson data first"
+            owner={
+              <div style={{ margin: '0 1rem', display: 'inline-block' }}>
+                <Link href={`${asPath}/play`} passHref>
+                  <Button
+                    rounded
+                    size="xl"
+                    as="a"
+                    color="#565656"
+                    bgColor="#97D492"
+                    text="Play"
+                    Icon={FaPlay}
+                    iconPosition="right"
+                    disabled={!isDataReady}
+                  />
+                </Link>
+              </div>
+            }
+          />
 
-        <Button
-          rounded
-          size="xl"
-          color="#565656"
-          bgColor="#F8F8F8"
-          text="Student Instructions"
-          Icon={FaArrowRight}
-          iconPosition="right"
-          onClick={() => openInstructionModal('student')}
-        />
-      </div>
+          <Button
+            rounded
+            size="xl"
+            color="#565656"
+            bgColor="#F8F8F8"
+            text="Student Instructions"
+            Icon={FaArrowRight}
+            iconPosition="right"
+            onClick={() => openInstructionModal('student')}
+          />
+        </div>
 
-      <div className="image">
-        <Image
-          isTransparent
-          src={imgUrl}
-          alt={title}
-          height={image.height * 3}
-          width={image.width * 3}
-        />
-      </div>
+        <div className="image">
+          <Image
+            isTransparent
+            src={imgUrl}
+            alt={title}
+            height={image.height * 3}
+            width={image.width * 3}
+          />
+        </div>
 
-      {!!warnings.length && (
-        <Block isStatic id="game_warnings" color="#ff4500" header="Warnings:">
+        {!!warnings.length && (
+          <Block isStatic id="game_warnings" color="#ff4500" header="Warnings:">
+            <ul style={{ paddingLeft: 30, margin: '1rem 0' }}>
+              {warnings.map(warning => (
+                <li key={warning.slice(0, 15)}>{warning}</li>
+              ))}
+            </ul>
+          </Block>
+        )}
+
+        <Block isStatic id="game_notes" color="#01918d" header="Please Note:">
           <ul style={{ paddingLeft: 30, margin: '1rem 0' }}>
-            {warnings.map(warning => (
-              <li key={warning.slice(0, 15)}>{warning}</li>
+            {gameNotes.map(note => (
+              <li key={note.slice(0, 15)}>{note}</li>
             ))}
           </ul>
         </Block>
-      )}
 
-      <Block isStatic id="game_notes" color="#01918d" header="Please Note:">
-        <ul style={{ paddingLeft: 30, margin: '1rem 0' }}>
-          {gameNotes.map(note => (
-            <li key={note.slice(0, 15)}>{note}</li>
-          ))}
-        </ul>
-      </Block>
+        {['teacher', 'student'].includes(query.instructions as string) && (
+          <GameInstructionsModal
+            isOpen
+            gameImgUrl={imgUrl}
+            instructions={
+              query.instructions === 'student'
+                ? instructions.forStudents
+                : instructions.forTeachers
+            }
+            seoTitle={seoTitle}
+          />
+        )}
 
-      {['teacher', 'student'].includes(query.instructions as string) && (
-        <GameInstructionsModal
-          isOpen
-          gameImgUrl={imgUrl}
-          instructions={
-            query.instructions === 'student'
-              ? instructions.forStudents
-              : instructions.forTeachers
+        <style jsx>{`
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
           }
-        />
-      )}
 
-      <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
+          .heading {
+            margin: 2rem auto 1.25rem;
+            text-align: center;
+            color: #414141;
+            font-size: 3.5rem;
+            font-weight: normal;
+          }
 
-        .heading {
-          margin: 2rem auto 1.25rem;
-          text-align: center;
-          color: #414141;
-          font-size: 3.5rem;
-          font-weight: normal;
-        }
+          .image {
+            width: 70%;
+            padding: 1.25rem 1rem 2rem;
+          }
 
-        .image {
-          width: 70%;
-          padding: 1.25rem 1rem 2rem;
-        }
-
-        .button_container {
-          margin: 1rem auto 2rem;
-        }
-      `}</style>
-    </div>
+          .button_container {
+            margin: 1rem auto 2rem;
+          }
+        `}</style>
+      </div>
+    </SeoWrapper>
   );
 }
 
@@ -193,6 +225,7 @@ export const getStaticProps = async ({
   return {
     props: {
       title: gameConfig.title,
+      description: gameConfig.description,
       image: gameConfig.image,
       instructions: gameConfig.instructions,
       warnings: gameConfig.warnings,
