0698b3668c8b003b75f88fd8a11d87d8938aff54
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-22 17:59:25 +0900

    to correctly show the Layout wrapper on errors we need to check for them

--- components/Layout/usePlayCheck.tsx
+++ components/Layout/usePlayCheck.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 
 // checks if we are on the play game's page
 export default function usePlayCheck(): boolean {
-  const { asPath } = useRouter();
+  const { asPath, pathname } = useRouter();
 
-  return asPath.endsWith('/play');
+  return pathname !== '/404' && asPath.endsWith('/play');
 }
