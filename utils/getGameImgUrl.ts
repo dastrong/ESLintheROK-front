f164dce1fb415ffb77c8665b615d8d24f89afc9a
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-22 17:54:41 +0900

    added another function to get OG images

--- utils/getGameImgUrl.ts
+++ utils/getGameImgUrl.ts
@@ -1,5 +1,12 @@
+const baseCloudUrl = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
+const folder = '/ESLintheROK';
+
 export const getGameImgUrl = (gameTitle: string) => {
-  const baseCloudUrl = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
   const noSpacesTitle = gameTitle.replace(/\s/g, '');
-  return `${baseCloudUrl}/Games/${noSpacesTitle}/GameCover.svg`;
+  return `${baseCloudUrl}${folder}/Games/${noSpacesTitle}/GameCover.svg`;
+};
+
+export const getGameOgImgUrl = (gameTitle: string, transformations: string) => {
+  const noSpacesTitle = gameTitle.replace(/\s/g, '');
+  return `${baseCloudUrl}${transformations}${folder}/Games/${noSpacesTitle}/GameCover.png`;
 };
