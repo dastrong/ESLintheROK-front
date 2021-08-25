909bb5959deb649ae231600b265ecf1b2b18e774
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-25 15:23:35 +0900

    added utils to get media files from cloudinary for games

--- utils/getCloudUrls.ts
+++ utils/getCloudUrls.ts
@@ -1,8 +1,19 @@
 import { createBookImgName, BookTitleCreator } from 'utils/createBookImgName';
 
+type FileType = 'image' | 'video';
+
+const imageExtensions = ['svg', 'png', 'jpg', 'webp', 'gif'];
+const videoExtensions = ['mp3', 'wav'];
+
+const getFileType = (file: string): FileType => {
+  if (imageExtensions.some(type => file.endsWith(type))) return 'image';
+  if (videoExtensions.some(type => file.endsWith(type))) return 'video';
+  throw new Error("Wasn't able to handle that file extension");
+};
+
 export const getCloudImgBase = (
   transformations?: string,
-  type: 'image' | 'video' = 'image'
+  type: FileType = 'image'
 ) => {
   return (
     process.env.NEXT_PUBLIC_CLOUDINARY_URL +
@@ -23,6 +34,12 @@ export const getGameOgImgUrl = (gameTitle: string, transformations: string) => {
   const baseUrl = getCloudImgBase(transformations);
   const noSpacesTitle = gameTitle.replace(/\s/g, '');
   return `${baseUrl}/Games/${noSpacesTitle}/GameCover.png`;
+};
+
+export const getGameFileUrl = (gameFile: string, transformations?: string) => {
+  const fileType = getFileType(gameFile);
+  const baseUrl = getCloudImgBase(transformations, fileType);
+  return `${baseUrl}/Games/${gameFile}`;
 };
 
 export const getBookCoverUrl: BookTitleCreator = (...args) => {
