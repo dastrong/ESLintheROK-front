c141fe4f1afe179a3a2c8c0cc99186b167924587
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-27 22:38:28 +0900

    init FontLoader component

---
+++ components/FontLoader/FontLoader.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Head from 'next/head';
+import { defaultFonts, FontType } from 'lib/fonts';
+
+type Props = {
+  extraFonts?: FontType[];
+};
+
+const googleURL = 'https://fonts.googleapis.com/css2?family=';
+const convertName = (name: string) => name.replace(' ', '+');
+
+export default function FontLoader({ extraFonts }: Props) {
+  return (
+    <Head>
+      {defaultFonts.map(font => (
+        <link
+          key={font.name}
+          rel="stylesheet"
+          href={`${googleURL}${convertName(font.name)}`}
+        />
+      ))}
+
+      {extraFonts.map(font => (
+        <link
+          key={font.name}
+          rel="stylesheet"
+          href={`${googleURL}${convertName(font.name)}`}
+        />
+      ))}
+    </Head>
+  );
+}
