11dd7805b106a4206da1a5ac9666bf15ba166cbe
DaStrong <danielstrong.ds@gmail.com>
2021-08-31 16:14:58 +0900

    removed fadeout animation since it doesn't work now?

--- components/Layout/Content.tsx
+++ components/Layout/Content.tsx
@@ -20,37 +20,34 @@ export default function Content({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   const transition = useTransition(router.pathname, {
-    from: {
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-      delay: 200,
-    },
-    leave: {
-      position: 'absolute',
-      opacity: 0,
-      top: 'calc(var(--navHeight) - 1)',
-      height: 'calc(100vh - var(--navHeight) - var(--footerHeight))',
-      left: 0,
-    },
-    config: {
-      duration: 200,
-    },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    // leave: {
+    //   position: 'absolute',
+    //   opacity: 0,
+    //   top: 'calc(var(--navHeight) - 1)',
+    //   height: 'calc(100vh - var(--navHeight) - var(--footerHeight))',
+    //   left: 0,
+    // },
+    config: { duration: 500 },
   });
 
-  const fragment = transition(style => (
-    <a.section
-      className={className}
-      style={{
-        ...style,
-        height: isPlayingGame ? '100vh' : 'inherit',
-        padding: isPlayingGame ? '0' : 'inherit',
-      }}
-    >
-      {children}
-    </a.section>
-  ));
+  const fragment = transition(
+    (style, item) =>
+      item === router.pathname && (
+        <a.section
+          key={item}
+          className={className}
+          style={{
+            ...style,
+            height: isPlayingGame ? '100vh' : 'inherit',
+            padding: isPlayingGame ? '0' : 'inherit',
+          }}
+        >
+          {children}
+        </a.section>
+      )
+  );
 
   return (
     <>
