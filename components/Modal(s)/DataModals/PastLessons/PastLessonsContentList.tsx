c1bc1c862042bfd23c2516df1a97f89c1be319b8
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-02 23:19:36 +0900

    initial past lesson item styling

--- components/Modal(s)/DataModals/PastLessons/PastLessonsContentList.tsx
+++ components/Modal(s)/DataModals/PastLessons/PastLessonsContentList.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { FaCheck, FaPlus, FaTrashAlt } from 'react-icons/fa';
+import {
+  FaCheck,
+  FaPlus,
+  FaRegEdit,
+  FaSearch,
+  FaShareAlt,
+  FaTrash,
+} from 'react-icons/fa';
 import Button from 'components/Button';
 
 export default function PastLessonsContentList() {
@@ -8,23 +15,115 @@ export default function PastLessonsContentList() {
   return (
     <ul>
       <li>
-        <Button
-          size="md"
-          rounded
-          Icon={checked ? FaCheck : FaPlus}
-          color="white"
-          bgColor={checked ? '#1a961a' : '#2185d0'}
-          onClick={() => setChecked(state => !state)}
-        />
-        <div></div>
-        <Button
-          size="sm"
-          rounded
-          Icon={FaTrashAlt}
-          color="white"
-          bgColor="#db2828"
-          onClick={() => console.log('Deleted')}
-        />
+        <div className="left_side">
+          <Button
+            size="lg"
+            rounded
+            Icon={checked ? FaCheck : FaPlus}
+            color="white"
+            bgColor={checked ? '#1a961a' : '#616161'}
+            onClick={() => setChecked(state => !state)}
+          />
+          <div className="text_container">
+            <h3>Sunday, April 11th 2021 at 16:57</h3>
+            <div>
+              <span>15 Vocabulary</span>
+              <span>·</span>
+              <span>7 Expressions</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Button
+            rounded
+            Icon={FaRegEdit}
+            color="white"
+            bgColor="#EF8D32"
+            onClick={() => console.log('Edited')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaSearch}
+            color="white"
+            bgColor="#C534EA"
+            onClick={() => console.log('Viewed')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaShareAlt}
+            color="white"
+            bgColor="#33C7E8"
+            onClick={() => console.log('Shared')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaTrash}
+            color="white"
+            bgColor="#F03C47"
+            onClick={() => console.log('Deleted')}
+            style={{ margin: 4 }}
+          />
+        </div>
+      </li>
+
+      <li>
+        <div className="left_side">
+          <Button
+            size="lg"
+            rounded
+            Icon={checked ? FaCheck : FaPlus}
+            color="white"
+            bgColor={checked ? '#1a961a' : '#616161'}
+            onClick={() => setChecked(state => !state)}
+          />
+          <div className="text_container">
+            <h3>Sunday, April 11th 2021 at 16:57</h3>
+            <div>
+              <span>15 Vocabulary</span>
+              <span>·</span>
+              <span>7 Expressions</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Button
+            rounded
+            Icon={FaRegEdit}
+            color="white"
+            bgColor="#EF8D32"
+            onClick={() => console.log('Edited')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaSearch}
+            color="white"
+            bgColor="#C534EA"
+            onClick={() => console.log('Viewed')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaShareAlt}
+            color="white"
+            bgColor="#33C7E8"
+            onClick={() => console.log('Shared')}
+            style={{ margin: 4 }}
+          />
+          <Button
+            rounded
+            Icon={FaTrash}
+            color="white"
+            bgColor="#F03C47"
+            onClick={() => console.log('Deleted')}
+            style={{ margin: 4 }}
+          />
+        </div>
       </li>
 
       <style jsx>{`
@@ -38,8 +137,41 @@ export default function PastLessonsContentList() {
           display: flex;
           justify-content: space-between;
           align-items: center;
-          padding: 0.5rem 0;
-          border-bottom: 1px solid purple;
+          padding: 0.75rem;
+          border-bottom: 1px solid #d6d6d6;
+        }
+
+        li:last-child {
+          border: none;
+        }
+
+        .left_side {
+          display: flex;
+          align-items: center;
+        }
+
+        .text_container {
+          margin-left: 1rem;
+        }
+
+        .text_container div {
+          color: #5e5e5e;
+        }
+
+        .text_container h3 {
+          margin: 0;
+          margin-bottom: 0.5rem;
+          font-size: 1.4rem;
+          color: #313131;
+        }
+
+        .text_container div span {
+          font-size: 1.1rem;
+        }
+
+        .text_container div span:nth-of-type(2) {
+          font-weight: bold;
+          margin-inline: 0.75rem;
         }
       `}</style>
     </ul>
