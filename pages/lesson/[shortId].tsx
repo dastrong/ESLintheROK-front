b98e5b64e59d9604b92b74feab3e72a2a1137ee1
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-21 18:16:32 +0900

    added page to capture and set shared lesson data

---
+++ pages/lesson/[shortId].tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+import { useStore } from 'contexts/store';
+import { swrFetch } from 'utils/fetchers';
+import SeoWrapper from 'components/SeoWrapper';
+import { PageHeading, PageSubHeading } from 'components/PageHeadings';
+import Button from 'components/Button';
+
+export default function SharedLessonPage() {
+  const router = useRouter();
+  const { storeDispatch } = useStore();
+
+  const { data, isValidating, error } = useSWR(
+    `/past-lesson/shortId/${router.query.shortId}`,
+    swrFetch,
+    {
+      isPaused: () => !router.query.shortId,
+      onSuccess: data =>
+        storeDispatch({
+          type: 'Set_Data',
+          vocabulary: data.vocabulary,
+          expressions: data.expressions,
+        }),
+    }
+  );
+
+  console.log(data, isValidating, error);
+
+  return (
+    <SeoWrapper
+      title="Shared Lesson"
+      description="Have a shared lesson code? Use it here to get and set that lesson's data."
+    >
+      <PageHeading>Shared Lesson</PageHeading>
+      <PageSubHeading>
+        {error ||
+          (!data && !error
+            ? 'Please wait a moment while we get that data for you.'
+            : `Lesson (${
+                data?.title || 'Untitled'
+              }) data has been set. Have a good class 👍`)}
+      </PageSubHeading>
+
+      <div>
+        <Link href="/games" passHref>
+          <Button
+            as="a"
+            color="white"
+            bgColor="#2b7cd0"
+            text="Go To Games Screen"
+            style={{ textAlign: 'center' }}
+          />
+        </Link>
+
+        <Button
+          inverted
+          color="white"
+          bgColor="#e428bc"
+          text="Customize Lesson Data"
+          onClick={() => {
+            storeDispatch({ type: 'Open_Data_Modal', dataModalName: 'edit' });
+          }}
+          style={{ marginLeft: 8 }}
+        />
+
+        <style jsx>{`
+          div {
+            display: flex;
+            justify-content: center;
+            width: 500px;
+            margin: 0 auto;
+          }
+        `}</style>
+      </div>
+    </SeoWrapper>
+  );
+}
