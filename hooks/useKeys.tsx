4bdf65edc35197ade197b3885ef4371d0c329c5e
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-25 10:14:05 +0900

    updated game hooks to not run functions when things are open

--- hooks/useKeys.tsx
+++ hooks/useKeys.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import { useStore } from 'contexts/store';
+import { useGifs } from 'contexts/gifs';
 import useEventListener from './useEventListener';
 
 // cb contains game specific key events
@@ -7,16 +8,17 @@ export default function useKeys(
   handleGame: () => void,
   cb?: (e: KeyboardEvent) => void
 ): void {
-  const { isMenuOpen } = useStore();
+  const { isMenuOpen, dataModalName, showSettings } = useStore();
+  const { show } = useGifs();
 
   const handleKeys = useCallback(
     (e: KeyboardEvent) => {
-      if (isMenuOpen) return;
+      if (isMenuOpen || dataModalName || showSettings || show) return;
       if (e.code === 'Space' || e.key === 'Enter') return handleGame();
       if (!cb) return;
       cb(e);
     },
-    [isMenuOpen, handleGame, cb]
+    [isMenuOpen, dataModalName, showSettings, show, handleGame, cb]
   );
 
   useEventListener('keydown', handleKeys);
--- hooks/useScroll.tsx
+++ hooks/useScroll.tsx
@@ -1,16 +1,18 @@
 import { useCallback } from 'react';
-import useEventListener from 'hooks/useEventListener';
 import { useStore } from 'contexts/store';
+import { useGifs } from 'contexts/gifs';
+import useEventListener from 'hooks/useEventListener';
 
 // cb contains game specific key events
 export default function useScroll(
   cb: (scrolledUp: boolean, wheelClicked: boolean) => void
 ) {
-  const { isMenuOpen } = useStore();
+  const { isMenuOpen, dataModalName, showSettings } = useStore();
+  const { show } = useGifs();
 
   const handleScroll = useCallback(
     ({ deltaY, buttons }) => {
-      if (isMenuOpen) return;
+      if (isMenuOpen || dataModalName || showSettings || show) return;
       const scrolledUp = deltaY < 0;
       const wheelClicked = buttons === 4;
       cb(scrolledUp, wheelClicked);
