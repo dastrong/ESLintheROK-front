c800ea35f54b0997c9273bc2927e92cbe62d9b10
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-28 21:31:19 +0900

    added a mobile notice

--- components/Modals/GlobalModals/GlobalModals.tsx
+++ components/Modals/GlobalModals/GlobalModals.tsx
@@ -5,6 +5,7 @@ import Lessons from './Lessons';
 import PastLessons from './PastLessons';
 import Settings from './Settings';
 import Gif from './Gif';
+import MobileNotice from './MobileNotice';
 
 export default function DataModals() {
   return (
@@ -15,6 +16,7 @@ export default function DataModals() {
       <PastLessons />
       <Settings />
       <Gif />
+      <MobileNotice />
     </>
   );
 }
