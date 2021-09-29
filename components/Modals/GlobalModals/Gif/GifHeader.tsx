d2f31ca3e4f65adde6cdb7ee46f007918eeab778
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-29 23:07:41 +0900

    made powered by GIPHY into a link

--- components/Modals/GlobalModals/Gif/GifHeader.tsx
+++ components/Modals/GlobalModals/Gif/GifHeader.tsx
@@ -6,13 +6,15 @@ import { getCloudImgBase } from 'utils/getCloudUrls';
 export default function GifHeader({ closeModal }: { closeModal: () => void }) {
   return (
     <Modal.Header closeModal={closeModal}>
-      <Image
-        isTransparent
-        src={getCloudImgBase() + '/PoweredBy_GIPHY.png'}
-        alt="powered by giphy"
-        width={200}
-        height={22}
-      />
+      <a href="https://giphy.com" target="_blank">
+        <Image
+          isTransparent
+          src={getCloudImgBase() + '/PoweredBy_GIPHY.png'}
+          alt="powered by giphy"
+          width={200}
+          height={22}
+        />
+      </a>
     </Modal.Header>
   );
 }
