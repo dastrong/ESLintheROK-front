5efe4f0804ee8b7eafcfb50c58a9cf5eaf80a266
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-02 01:06:46 +0900

    removed old CRA files

--- src/components/reusable/Notebook/NotebookVertLine.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// double vertical red lines
-const NotebookVertLine = () => <div className="notebook-vert-line"></div>;
-
-export default NotebookVertLine;
--- src/helpers/ga.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from "react-ga";
-
-export function googleEvent(title) {
-  ReactGA.event({
-    category: "Games",
-    action: `New Round - ${title}`,
-    label: title,
-  });
-}
