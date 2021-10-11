526a14337bf1e04f920d4f4cbe2c8e61693370cd
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-11 19:49:14 +0900

    modified useKeys to allow skipping Space and Enter

--- hooks/useKeys.tsx
+++ hooks/useKeys.tsx
@@ -6,7 +6,8 @@ import useEventListener from './useEventListener';
 // cb contains game specific key events
 export default function useKeys(
   handleGame: () => void,
-  cb?: (e: KeyboardEvent) => void
+  cb?: (e: KeyboardEvent) => void,
+  skip: string[] = []
 ): void {
   const { isMenuOpen, dataModalName, showSettings } = useStore();
   const { show } = useGifs();
@@ -14,7 +15,11 @@ export default function useKeys(
   const handleKeys = useCallback(
     (e: KeyboardEvent) => {
       if (isMenuOpen || dataModalName || showSettings || show) return;
-      if (e.code === 'Space' || e.key === 'Enter') return handleGame();
+      if (
+        (e.code === 'Space' && !skip.includes('Space')) ||
+        (e.key === 'Enter' && !skip.includes('Enter'))
+      )
+        return handleGame();
       if (!cb) return;
       cb(e);
     },
