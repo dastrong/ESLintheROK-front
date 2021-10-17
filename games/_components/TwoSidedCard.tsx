e6b99faad91e6ac098c017d493744ef86ef0e9b8
Daniel Strong <danielstrong.ds@gmail.com>
2021-10-17 11:31:55 +0900

    corrected dynamic styled-jsx use and styled further

--- games/_components/TwoSidedCard.tsx
+++ games/_components/TwoSidedCard.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { forwardRef } from 'react';
-import FitText from 'components/FitText';
+import React, { forwardRef, useMemo } from 'react';
 import classNames from 'classnames';
+import { lighten } from 'color2k';
+import FitText from 'components/FitText';
 
 type TwoSidedCardProps = {
   textFront: string;
@@ -43,6 +44,8 @@ const TwoSidedCard = forwardRef<HTMLSpanElement, TwoSidedCardProps>(
       [cardClass]: flipX || flipY || slideOut,
     });
 
+    const shadowColor = useMemo(() => lighten(colorFront, 0.5), [colorFront]);
+
     return (
       <div className={cx} id={id} onClick={handleClick}>
         {/* FRONT SIDE */}
@@ -57,21 +60,14 @@ const TwoSidedCard = forwardRef<HTMLSpanElement, TwoSidedCardProps>(
           div {
             font-size: 8vw;
             color: #fff;
-            text-shadow: 2px 2px 1px black;
+            text-shadow: 0px 0px 1px #333, 0px 0px 4px #555, 0px 0px 7px #777,
+              0px 0px 10px #777777b4, 0px 0px 14px #7777772f;
             transform-style: preserve-3d;
             height: 100%;
             width: 100%;
             user-select: none;
             display: inline-block;
             position: relative;
-          }
-
-          .two-sided-card {
-            width: ${width};
-            height: ${height};
-            flex: ${slideOut === undefined ? '' : slideOut ? 0 : 1};
-            transform: rotateX(${flipX && '180deg'});
-            transform: rotateY(${flipY && '180deg'});
           }
 
           .two-sided-card_front,
@@ -83,12 +79,27 @@ const TwoSidedCard = forwardRef<HTMLSpanElement, TwoSidedCardProps>(
             align-items: center;
             justify-content: center;
           }
+        `}</style>
 
+        <style jsx>{`
+          .two-sided-card {
+            width: ${width};
+            height: ${height};
+            flex: ${slideOut === undefined ? '' : slideOut ? 0 : 1};
+            transform: rotateX(${flipX && '180deg'});
+            transform: rotateY(${flipY && '180deg'});
+          }
+        `}</style>
+
+        <style jsx>{`
           .two-sided-card_front {
             background-color: ${colorFront};
+            box-shadow: 0 0 1vw 0 ${shadowColor} inset;
             cursor: pointer;
           }
+        `}</style>
 
+        <style jsx>{`
           .two-sided-card_back {
             background-color: ${colorBack};
             cursor: default;
