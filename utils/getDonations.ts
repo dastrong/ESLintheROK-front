fefaa9c7c2b13ce7666c936188846dd0d5c99157
Daniel <daniel@code-inspector.com>
2022-05-29 12:08:44 -0700

    fix: return an empty array if an error happened

--- utils/getDonations.ts
+++ utils/getDonations.ts
@@ -5,7 +5,12 @@ const options = { headers: { Authorization: `Bearer ${token}` } };
 
 // reusable fetcher that's preconfigured for the buymeacoffee api
 const fetcher = async (url: string) => {
-  return await fetch(url, options).then(r => r.json());
+  return await fetch(url, options).then(resp => {
+    if (resp.status === 403) {
+      throw new Error('Forbidden');
+    }
+    return resp.json();
+  });
 };
 
 // gets every transaction for the given urls endpoint
@@ -13,22 +18,26 @@ export const getDonations = async (addUrl: string) => {
   // add the given url param to the base url
   const url = baseUrl + addUrl;
 
-  // returns only the first page results and some additional data
-  const firstPageResults = await fetcher(url);
+  try {
+    // returns only the first page results and some additional data
+    const firstPageResults = await fetcher(url);
 
-  // find out how many other result pages there is
-  const otherPageCount = firstPageResults.last_page - 1;
+    // find out how many other result pages there is
+    const otherPageCount = firstPageResults.last_page - 1;
 
-  // create an array of urls to fetch
-  const otherPageUrls = Array(otherPageCount)
-    .fill('')
-    .map((_, i) => `${url}?page=${i + 2}`);
+    // create an array of urls to fetch
+    const otherPageUrls = Array(otherPageCount)
+      .fill('')
+      .map((_, i) => `${url}?page=${i + 2}`);
 
-  // fetch all the other page results and combine with the first page
-  const allPageResults = await Promise.all(otherPageUrls.map(fetcher))
-    .then(results => results.map(({ data }) => data))
-    .then(otherResults => [firstPageResults.data, ...otherResults])
-    .then(allResults => allResults.reduce((acc, cVal) => [...acc, ...cVal]));
+    // fetch all the other page results and combine with the first page
+    const allPageResults = await Promise.all(otherPageUrls.map(fetcher))
+      .then(results => results.map(({ data }) => data))
+      .then(otherResults => [firstPageResults.data, ...otherResults])
+      .then(allResults => allResults.reduce((acc, cVal) => [...acc, ...cVal]));
 
-  return allPageResults;
+    return allPageResults;
+  } catch (err) {
+    return [];
+  }
 };
